05a5545660981ef69c311c5257a5e8054ae7ee26
Karen Casey-Gover <karen.casey@hotmail.com>
2016-11-09 01:26:35 +0000

    completed exercise 1-4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store1 = Store.create(name: 'burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: 'richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store3 = Store.create(name: 'gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count(:name)
+
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: 'surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: 'whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: 'yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts " #{ store.name }'s annual revenue is #{ store.annual_revenue }"
+end
+
+@womens_stores = Store.where("annual_revenue < 1000000 AND womens_apparel = true")
+@womens_stores.each do |store|
+  puts " #{ store.name }'s annual revenue is #{ store.annual_revenue }"
+end
