dcfe8c6c9a3ba27b18f5ce5c178e3d694d9893c1
Karen Casey-Gover <karen.casey@hotmail.com>
2016-11-14 02:05:44 +0000

    fixed 2 typos in validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,7 +2,7 @@ class Employee < ActiveRecord::Base
   belongs_to :store
 
   validates :first_name, :last_name, :store_id, presence: true
-  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, :less_than: 200 }
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
 
 end
 #puts Employee.count(:first_name)
